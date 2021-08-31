cac9453806539fb3919be499bd14404993e73dae
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2021-08-31 03:22:25 +0300

    Add prints

--- patch.py
+++ patch.py
@@ -23,6 +23,7 @@ FOLDERS_TO_COPY = [
 
 def main():
     command = sys.argv[0]
+    print(command)
     if command == 'a' or command == 'apply':
         apply()
     if command == 'u' or command == 'unapply':
@@ -30,11 +31,13 @@ def main():
 
 
 def apply():
+    print('Applying patch...')
     create_backup(FILES_TO_BACKUP)
     copy_src_to_dest(FILES_TO_COPY, FOLDERS_TO_COPY)
 
 
 def unapply():
+    print('Unapplying patch...')
     delete_copied_files_and_folders(FILES_TO_COPY, FOLDERS_TO_COPY)
     restore_backup(FILES_TO_BACKUP)
 
@@ -42,26 +45,32 @@ def unapply():
 def create_backup(files):
     BACKUP_ROOT.mkdir(parents=True, exist_ok=True)
     for file in files:
-        shutil.move(SRC_ROOT / file, BACKUP_ROOT / file)
+        shutil.move(SRC_ROOT / file, BACKUP_ROOT)
+        print(f'Moved file {SRC_ROOT / file} to {BACKUP_ROOT}')
 
 
 def copy_src_to_dest(files, folders):
     for file in files:
         shutil.copy(SRC_ROOT / file, DEST_ROOT)
+        print(f'Copied file {SRC_ROOT / file} to {DEST_ROOT}')
     for folder in folders:
         shutil.copytree(SRC_ROOT / folder, DEST_ROOT)
+        print(f'Copied folder {SRC_ROOT / folder} to {DEST_ROOT}')
 
 
 def delete_copied_files_and_folders(files, folders):
     for file in files:
         (DEST_ROOT / file).unlink()
+        print(f'Deleted file {DEST_ROOT / file}')
     for folder in folders:
         (DEST_ROOT / folder).rmdir()
+        print(f'Deleted folder {DEST_ROOT / folder}')
 
 
 def restore_backup(files):
     for file in files:
         shutil.copy(BACKUP_ROOT / file, DEST_ROOT)
+        print(f'Copied backup file {BACKUP_ROOT / file} to {DEST_ROOT}')
 
 
 if __name__ == '__main__':
